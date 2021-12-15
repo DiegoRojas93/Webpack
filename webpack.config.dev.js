ff89f71cb4aef41bbdb1ee382265fb3184e3a7e1
Diego Rojas <diegorojas431@gmail.com>
2021-12-14 21:03:39 -0500

    Modo de desarrollo

---
+++ webpack.config.dev.js
@@ -0,0 +1,83 @@
+// ./webpack.config.dev.js
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin'),
+      MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+      CopyPlugin = require('copy-webpack-plugin'),
+      Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  mode:'development',
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css|.styl$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000,
+            mimeType: 'application/font-woff',
+            name: "[name].[contenthash].[ext]",
+            outputPath: './assets/fonts/',
+            publicPath: './assets/fonts/',
+            esModule: false
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    }),
+    new Dotenv()
+  ]
+}
