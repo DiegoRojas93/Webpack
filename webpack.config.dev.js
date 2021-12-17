fd9af6e5cabf41b5666ae513565deb204806d45b
Diego Rojas <diegorojas431@gmail.com>
2021-12-16 21:12:21 -0500

    Webpack dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,29 +1,34 @@
 // ./webpack.config.dev.js
 
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin'),
-      MiniCssExtractPlugin = require('mini-css-extract-plugin'),
-      CopyPlugin = require('copy-webpack-plugin'),
-      Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin"),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  CopyPlugin = require("copy-webpack-plugin"),
+  Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: `./src/index.js`,
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.[contenthash].js',
-    assetModuleFilename: 'assets/images/[hash][ext][query]'
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.[contenthash].js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
-  mode:'development',
-  watch: true,
+  mode: "development",
+  devServer: {
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true
+  },
   resolve: {
-    extensions: ['.js'],
+    extensions: [".js"],
     alias: {
-      '@utils': path.resolve(__dirname, 'src/utils/'),
-      '@templates': path.resolve(__dirname, 'src/templates/'),
-      '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@images': path.resolve(__dirname, 'src/assets/images/'),
-    }
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
   },
   module: {
     rules: [
@@ -31,54 +36,50 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css|.styl$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'stylus-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2)$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
             limit: 1000,
-            mimeType: 'application/font-woff',
+            mimeType: "application/font-woff",
             name: "[name].[contenthash].[ext]",
-            outputPath: './assets/fonts/',
-            publicPath: './assets/fonts/',
-            esModule: false
-          }
-        }
-      }
-    ]
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: './public/index.html',
-      filename: './index.html'
+      template: "./public/index.html",
+      filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].[contenthash].css'
+      filename: "assets/[name].[contenthash].css",
     }),
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, "src", "assets/images"),
-          to: "assets/images"
-        }
-      ]
+          to: "assets/images",
+        },
+      ],
     }),
-    new Dotenv()
+    new Dotenv(),
   ]
-}
+};
